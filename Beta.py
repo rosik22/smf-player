7530433abe340a8b543b23bc6f15c5fe70058e51
Viktor Yankov <roterabe@yahoo.com>
2020-05-24 14:47:48 +0300

    Base making for mp3 player.

---
+++ Beta.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import wx
+import os
+import sys
+import time
+import sqlite3
+from mutagen.mp3 import MP3
+from pygame import mixer
+
+
+class Scope(wx.Frame):
+    def __init__(self, parent, id):
+
+        no_resize = wx.DEFAULT_FRAME_STYLE & ~ (wx.RESIZE_BORDER |
+                                                wx.MAXIMIZE_BOX)
+
+        super().__init__(
+            None, title="Scope", style=no_resize, size=(1024, 1024), pos=(0, 0))
+        self.SetBackgroundColour("White")
+        self.panel = wx.Panel(self)
+        self.panel.SetBackgroundColour("Gray")
+
+        menubar = wx.MenuBar()
+        filemenu = wx.Menu()
+        open = wx.MenuItem(filemenu, wx.ID_OPEN, '&Open')
+        exit = wx.MenuItem(filemenu, wx.ID_CLOSE, '&Exit')
+        filemenu.Append(open)
+        filemenu.Append(exit)
+        menubar.Append(filemenu, '&File')
+        self.SetMenuBar(menubar)
+        self.Bind(wx.EVT_MENU, self.menuhandler)
+
+        self.items = wx.BoxSizer(wx.HORIZONTAL)
+
+    def menuhandler(self, event):
+        id = event.GetId()
+        if id == wx.ID_OPEN:
+            with wx.FileDialog(self.panel, "Open Image file", wildcard="Music files (*.mp3,*.wav,*.aac,*.ogg)|*.mp3;*.wav;*.aac;*.ogg",
+                               style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as file:
+
+                if file.ShowModal() == wx.ID_CANCEL:
+                    return
+
+                self.pathname = file.GetPath()
+                try:
+                    #TODO Allow the loading of the file
+
+                    loadfile(self.pathname)
+
+                except IOError:
+                    wx.LogError("Cannot open file '%s'." % newfile)
+
+        if id == wx.ID_CLOSE:
+            self.Close()
+
+    def loadfile(self, path):
+        #TODO implement file load
+
+    def playlistd(self, path):
+        #TODO Implement playlist adding to playing now.
+
+
+app = wx.App()
+frame = Scope(None, -1)
+frame.Show()
+app.MainLoop()
