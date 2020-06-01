cad88ab9f6498d76906709e01553c14c6f7517ac
Viktor Yankov <roterabe@yahoo.com>
2020-06-02 02:45:56 +0300

    Now caching recommendations for major speed improvement.

--- main/Beta.py
+++ main/Beta.py
@@ -11,6 +11,8 @@ import urllib.request
 import urllib.parse
 import json
 import spotipy
+import wave
+import contextlib
 from spotipy.oauth2 import SpotifyClientCredentials
 from mutagen.id3 import ID3
 from mutagen import File as MutaFile
@@ -27,6 +29,7 @@ os.environ['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:9090'
 
 # Currently loaded songs.
 currentpl = 'playing.db'
+recommendation_data = 'rec.db'
 
 
 class Scope(wx.Frame):
@@ -37,7 +40,7 @@ class Scope(wx.Frame):
         super().__init__(
             None, title="Scope", style=no_resize, size=(600, 920), pos=(0, 0))
 
-        self.establishConnection()
+        self.establishConnectionRun()
 
         self.SetBackgroundColour("Black")
         self.countListCttl = 0
@@ -79,6 +82,7 @@ class Scope(wx.Frame):
         self.recBox.Bind(wx.EVT_LIST_ITEM_SELECTED,
                          self.loadSongFromRecommendationBox)
         self.rec.SetBackgroundColour("Gray")
+        self.recommendations = []
 
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.onTimer)
@@ -194,6 +198,7 @@ class Scope(wx.Frame):
 
 #-----------------------------------------------------------------------------------------------------------------------#
     def loadSong(self, row):
+        self.clearRecommendationBox()
         d = []
         cols = self.playlistBox.GetColumnCount()
         for col in range(cols-1):
@@ -213,17 +218,26 @@ class Scope(wx.Frame):
         self.setTimesPlayed(path, row)
         self.ButtonPlay.SetValue(True)
         self.makeCover(songTitle, artistName, path)
-        try:
-            self.clearRecommendationBox()
-            self.getSongRecommendation(songTitle, artistName)
-           # self.songrec(self.song_name, self.artist_name)
-        except:
-            print("Could not load any recommendations with LastFM/Spotify combo..")
-            print("Trying long query of Spotify Web API...")
+        found = False
+        for recs in self.recommendations:
+            for x in recs:
+                if artistName == x[3]:
+                    self.fillRecommendationBox(recs, artistName)
+                    found = True
+                    print(recs[3])
+                    break
+        if found is False:
             try:
-                self.songrec(songTitle, artistName)
+                self.getSongRecommendation(songTitle, artistName)
+                # self.songrec(self.song_name, self.artist_name)
             except:
-                print("No recommendations for the current title...")
+                print("Could not load any recommendations with LastFM/Spotify combo..")
+                print("Trying long query of Spotify Web API...")
+                try:
+                    self.songrec(songTitle, artistName)
+                except:
+                    print("No recommendations for the current title...")
+
 #-----------------------------------------------------------------------------------------------------------------------#
 
     def loadSongFromRecommendationBox(self, e):
@@ -237,11 +251,12 @@ class Scope(wx.Frame):
 
         artistName = str(d[0])
         songTitle = str(d[1])
-        for song in self.recommendations:
-            if song[0] == artistName and song[1] == songTitle:
-                self.Player.LoadURI(song[2])
-                self.Player.Play()
-                break
+        for s in self.recommendations:
+            for song in s:
+                if song[0] == artistName and song[1] == songTitle:
+                    self.Player.LoadURI(song[2])
+                    self.Player.Play()
+                    break
 
 #-----------------------------------------------------------------------------------------------------------------------#
     def setTimesPlayed(self, path, row):
@@ -288,8 +303,14 @@ class Scope(wx.Frame):
 #-----------------------------------------------------------------------------------------------------------------------#
     def getMutagenTags(self, path):
         data = []
-        song = MutaFile(path)
-        d = int(song.info.length)
+        try:
+            song = MutaFile(path)
+            d = int(song.info.length)
+        except:
+            with contextlib.closing(wave.open(path, 'r')) as file:
+                frames = file.getnframes()
+                rate = file.getframerate()
+                d = frames / float(rate)
         title = 'n/a'
         artist = 'n/a'
         backup_name = os.path.split(path)
@@ -309,19 +330,21 @@ class Scope(wx.Frame):
         url += str(d)
         url += '&fingerprint='
         url += fing
-        text = urllib.request.urlopen(url)
-        parsed = json.loads(text.read())
-        names = list(acoustid.parse_lookup_result(parsed))
-        for x in names:
-            if None not in x:
-                names = x
-                title = names[-2]
-                artist = names[-1]
-                if ';' in artist:
-                    artist = artist.split(';')
-                    artist = artist[0]
-                break
-
+        try:
+            text = urllib.request.urlopen(url)
+            parsed = json.loads(text.read())
+            names = list(acoustid.parse_lookup_result(parsed))
+            for x in names:
+                if None not in x:
+                    names = x
+                    title = names[-2]
+                    artist = names[-1]
+                    if ';' in artist:
+                        artist = artist.split(';')
+                        artist = artist[0]
+                    break
+        except:
+            print("No data...")
         # Check if file has ID3 tags. If not, use the LastFM API for naming.
         try:
             audio = ID3(path)
@@ -334,8 +357,8 @@ class Scope(wx.Frame):
             song_year = ''
 
         # Insert song data in list for inserting in database of currently playing songs.
-        minutes = d // 60
-        seconds = d % 60
+        minutes = int(d // 60)
+        seconds = int(d % 60)
         duration = str(minutes) + ":" + str(seconds)
 
         data.append(self.song_name)
@@ -371,20 +394,21 @@ class Scope(wx.Frame):
         self.countListCttl += 1
 
 #-----------------------------------------------------------------------------------------------------------------------#
-    def fillRecommendationBox(self, data):
+    def fillRecommendationBox(self, data, artist_name):
         dur = '0:30'
-        list1 = (data[0], data[1], dur)
-        self.recBox.InsertItem(0, str(list1[0]))
-        self.recBox.SetItem(0, 1, str(list1[1]))
-        self.recBox.SetItem(0, 2, str(list1[2]))
+        for x in data:
+            if artist_name in x:
+                list1 = (x[0], x[1], dur)
+                self.recBox.InsertItem(0, str(list1[0]))
+                self.recBox.SetItem(0, 1, str(list1[1]))
+                self.recBox.SetItem(0, 2, str(list1[2]))
 
 #-----------------------------------------------------------------------------------------------------------------------#
     def clearRecommendationBox(self):
         self.recBox.DeleteAllItems()
-        self.recommendations = []
 
 #-----------------------------------------------------------------------------------------------------------------------#
-    def establishConnection(self):
+    def establishConnectionRun(self):
         self.conn = None
         try:
             self.conn = sqlite3.connect(currentpl)
@@ -393,10 +417,10 @@ class Scope(wx.Frame):
             print("Unable to establish connection to database...\n")
 
         self.curs = self.conn.cursor()
-        self.createTable()
+        self.createTableRunning()
 
 #-----------------------------------------------------------------------------------------------------------------------#
-    def createTable(self):
+    def createTableRunning(self):
         self.curs.execute('''CREATE TABLE IF NOT EXISTS playlist
                             (title VARCHAR(255) UNIQUE,
                             duration VARCHAR(255),
@@ -460,7 +484,6 @@ class Scope(wx.Frame):
 #-----------------------------------------------------------------------------------------------------------------------#
     def getSongRecommendation(self, track_name, artist_name):
         # Get album name for reference from LastFM API.
-
         uurl = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&limit=10&api_key=5240ab3b0de951619cb54049244b47b5&format=json&artist='
         uurl += urllib.parse.quote(artist_name) + \
             '&track=' + urllib.parse.quote(track_name)
@@ -468,56 +491,61 @@ class Scope(wx.Frame):
         uurlink = urllib.request.urlopen(uurl)
         pparsed = json.load(uurlink)
         album_name = pparsed['track']['album']['title']
-        # print(pparsed['track']['album']['title'])
-        # print(album_name)
 
+        recommendations = []
         sp = spotipy.Spotify(
             client_credentials_manager=SpotifyClientCredentials())
 
         album_search = sp.search(q=album_name, limit=50, type='album')
         for album in album_search['albums']['items']:
             album_name_sp = album['name']
-            # print(album_name_sp)
-            # print(album['artists'][0]['external_urls']['spotify'])
+
         try:
             self.artist_url = ''
             results = sp.search(q=artist_name, limit=20, type='artist')
+
             for artists in results['artists']['items']:
                 if self.artist_url != '':
                     break
+
                 if artist_name.lower() == str(artists['name']).lower():
                     artist_url = artists['external_urls']['spotify']
+
                     for album in album_search['albums']['items']:
-                        # album_name_sp = album['name']
                         artist_url1 = album['artists'][0]['external_urls']['spotify']
+
                         if artist_url == artist_url1:
                             self.artist_url = artist_url1
                             break
+
             self.artist_url = str(self.artist_url)
-            # print(self.artist_url)
             self.artist_url = self.artist_url.split('artist/', 1)
             self.artist_url = self.artist_url[1]
             artist_seed = []
             artist_seed.append(self.artist_url)
             rec = sp.recommendations(seed_artists=artist_seed, limit=20)
+
             if len(rec['tracks']) == 0:
                 raise Exception
+
             for track in rec['tracks']:
-                # print(len(rec['tracks']))
                 if track['preview_url'] is not None:
+
                     preview_url = track['preview_url']
                     title = track['name']
                     # print(str(track['preview_url']) + ' -- ' + track['name'])
                     art_name = track['album']['artists'][0]['name']
-                    data = [art_name, title, preview_url]
-                    self.recommendations.append(data)
-                    self.fillRecommendationBox(data)
+                    data = [art_name, title, preview_url, artist_name]
+                    recommendations.append(data)
+            self.recommendations.append(recommendations)
+            self.fillRecommendationBox(recommendations, artist_name)
+
         except:
             print("Trying LastFM only as reference.")
             url = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&limit=10&api_key=5240ab3b0de951619cb54049244b47b5&format=json&artist='
             url += urllib.parse.quote(artist_name) + \
                 '&track=' + urllib.parse.quote(track_name)
-            # print(url)
+
             link = urllib.request.urlopen(url)
             parsed = json.load(link)
             if len(parsed['similartrack']['tracks']) == 0:
@@ -528,10 +556,13 @@ class Scope(wx.Frame):
 
 #-----------------------------------------------------------------------------------------------------------------------#
 
+
     def songrec(self, track_name, artist_name):
         artist_url = ''
+        recommendations = []
         sp = spotipy.Spotify(
             client_credentials_manager=SpotifyClientCredentials())
+
         found = False
         off = 0
         while (found is False or off < 100):
@@ -541,32 +572,36 @@ class Scope(wx.Frame):
             for track in track_search['tracks']['items']:
                 sp_track_name = track['name']
                 sp_artist_name = track['album']['artists'][0]['name']
-                # print(sp_track_name + ' -- ' +
-                #      sp_artist_name + ' || ' + track_name)
+
                 if str(track_name).lower() == str(sp_track_name).lower() and str(artist_name).lower() == str(sp_artist_name).lower():
                     artist_url = track['album']['artists'][0]['external_urls']['spotify']
                     found = True
                     print(str(artist_url))
                     break
+
             off += 50
+
         artist_url = str(artist_url)
         artist_url = artist_url.split('artist/', 1)
         artist_url = artist_url[1]
         artist_seed = []
         artist_seed.append(artist_url)
         rec = sp.recommendations(seed_artists=artist_seed, limit=20)
+
         if len(rec['tracks']) == 0:
             raise Exception("No recommendations")
+
         for track in rec['tracks']:
-            # print(len(rec['tracks']))
             if track['preview_url'] is not None:
+
                 preview_url = track['preview_url']
                 title = track['name']
-                # print(str(track['preview_url']) + ' -- ' + track['name'])
                 art_name = track['album']['artists'][0]['name']
-                data = [art_name, title, preview_url]
-                self.recommendations.append(data)
-                self.fillRecommendationBox(data)
+                data = [art_name, title, preview_url, artist_name]
+                recommendations.append(data)
+
+        self.recommendations.append(recommendations)
+        self.fillRecommendationBox(recommendations, artist_name)
 
 #-----------------------------------------------------------------------------------------------------------------------#
     def OnNext(self, event):
